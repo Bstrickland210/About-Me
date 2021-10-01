51e3602009a7d9c90472bc7740fa4a3c09f6afbd
Bridger Strickland <bridger210strick@gmail.com>
2021-10-01 09:57:09 -0600

    Initial Commit

---
+++ About Me/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  About Me
+//
+//  Created by Bridger Strickland on 10/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
